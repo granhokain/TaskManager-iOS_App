03853ddcd738bb80dbb982fb8d9b11452939414d
Rafael Martins <granhokain@gmail.com>
2019-04-18 10:22:40 -0300

    Initial Commit

---
+++ TaskManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Rafael Martins on 18/04/19.
+//  Copyright © 2019 Rafael Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
